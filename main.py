ffc69caa912f6b4ec0d591b5c86976b451b32ba3
Pavel <pasha20030407@gmail.com>
2022-05-03 17:49:42 +0300

    task2

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from student import Student
 def get_result(students):
     result = []
     for student in students:
-        flag = True
+        flag = False
+        flag2 = False
+        flag3 = True
         for grade in student.grades:
-            if (grade != 5):
-                flag = False
-        if (flag):
+            if (grade == 5):
+                flag = True
+            if (grade == 4):
+                flag2 = True
+            if (grade <= 3):
+                flag3 = False
+        if (flag & flag2 & flag3):
             result.append(student)
 
     return result
